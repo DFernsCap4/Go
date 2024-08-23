b7e01977cd9ef0af7dce2d15723e04c9967d0d74
Cwlass <142505558+DFernsCap4@users.noreply.github.com>
2024-08-23 11:17:30 +0200

    clean crud

--- Api/handlers/album.go
+++ Api/handlers/album.go
@@ -6,25 +6,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var albums = []models.Album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-}
+// var albums = []models.Album{
+// 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+//     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+//     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+// }
 
 func GetAlbums(c *gin.Context){
+	albums, err := models.GetAlbums()
+	if err != nil{
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
 func GetAlbumsById(c *gin.Context){
 	id := c.Param("id")
 
-	for _,value := range albums{
-		if value.ID == id {
-			c.IndentedJSON(http.StatusOK, value)
+	album, err := models.GetAlbumsById(id)
+	if err != nil {
+		if err.Error() == "album not found" {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Album not found"})
+		} else {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		}
+		return
 	}
-
+	c.IndentedJSON(http.StatusOK, album)
 }
 
 func PostAlbums(c *gin.Context){
@@ -34,5 +43,9 @@ func PostAlbums(c *gin.Context){
 		return
 	}
 
-	albums = append(albums, newAlbum)
+	if err := newAlbum.AddAlbum(); err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		return
+	}
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
